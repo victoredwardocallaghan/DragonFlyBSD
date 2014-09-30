c11aac61dbbd4574d788bc74139391abfad421d7
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-09-30 22:03:13 +1000

    sys/cddl: Lockup 'opensolaris_atomic.c' with lockmgr
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- sys/cddl/compat/opensolaris/kern/opensolaris_atomic.c
+++ sys/cddl/compat/opensolaris/kern/opensolaris_atomic.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2007 Pawel Jakub Dawidek <pjd@FreeBSD.org>
+ * Copyright (c) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -35,40 +36,46 @@ __FBSDID("$FreeBSD$");
 #ifdef _KERNEL
 #include <sys/kernel.h>
 
-struct mtx atomic_mtx;
-MTX_SYSINIT(atomic, &atomic_mtx, "atomic", MTX_DEF);
+struct lock atomic_lock;
+#define	mtx_lock(lock)		lockmgr((lock), LK_EXCLUSIVE)
+#define	mtx_unlock(lock)	lockmgr((lock), LK_RELEASE)
+
 #else
 #include <pthread.h>
 
 #define	mtx_lock(lock)		pthread_mutex_lock(lock)
 #define	mtx_unlock(lock)	pthread_mutex_unlock(lock)
 
-static pthread_mutex_t atomic_mtx;
+static pthread_mutex_t atomic_lock;
+#endif
 
 static __attribute__((constructor)) void
 atomic_init(void)
 {
-	pthread_mutex_init(&atomic_mtx, NULL);
-}
+#ifdef _KERNEL
+	lockinit(&atomic_lock, "atomic_lock", 0, LK_CANRECURSE);
+#else
+	pthread_mutex_init(&atomic_lock, NULL);
 #endif
+}
 
 #if !defined(__LP64__) && !defined(__mips_n32) && !defined(ARM_HAVE_ATOMIC64)
 void
 atomic_add_64(volatile uint64_t *target, int64_t delta)
 {
 
-	mtx_lock(&atomic_mtx);
+	mtx_lock(&atomic_lock);
 	*target += delta;
-	mtx_unlock(&atomic_mtx);
+	mtx_unlock(&atomic_lock);
 }
 
 void
 atomic_dec_64(volatile uint64_t *target)
 {
 
-	mtx_lock(&atomic_mtx);
+	mtx_lock(&atomic_lock);
 	*target -= 1;
-	mtx_unlock(&atomic_mtx);
+	mtx_unlock(&atomic_lock);
 }
 #endif
 
@@ -77,9 +84,9 @@ atomic_add_64_nv(volatile uint64_t *target, int64_t delta)
 {
 	uint64_t newval;
 
-	mtx_lock(&atomic_mtx);
+	mtx_lock(&atomic_lock);
 	newval = (*target += delta);
-	mtx_unlock(&atomic_mtx);
+	mtx_unlock(&atomic_lock);
 	return (newval);
 }
 
@@ -87,9 +94,9 @@ atomic_add_64_nv(volatile uint64_t *target, int64_t delta)
 void
 atomic_or_8(volatile uint8_t *target, uint8_t value)
 {
-	mtx_lock(&atomic_mtx);
+	mtx_lock(&atomic_lock);
 	*target |= value;
-	mtx_unlock(&atomic_mtx);
+	mtx_unlock(&atomic_lock);
 }
 #endif
 
@@ -98,9 +105,9 @@ atomic_or_8_nv(volatile uint8_t *target, uint8_t value)
 {
 	uint8_t newval;
 
-	mtx_lock(&atomic_mtx);
+	mtx_lock(&atomic_lock);
 	newval = (*target |= value);
-	mtx_unlock(&atomic_mtx);
+	mtx_unlock(&atomic_lock);
 	return (newval);
 }
 
@@ -109,11 +116,11 @@ atomic_cas_64(volatile uint64_t *target, uint64_t cmp, uint64_t newval)
 {
 	uint64_t oldval;
 
-	mtx_lock(&atomic_mtx);
+	mtx_lock(&atomic_lock);
 	oldval = *target;
 	if (oldval == cmp)
 		*target = newval;
-	mtx_unlock(&atomic_mtx);
+	mtx_unlock(&atomic_lock);
 	return (oldval);
 }
 
@@ -122,11 +129,11 @@ atomic_cas_32(volatile uint32_t *target, uint32_t cmp, uint32_t newval)
 {
 	uint32_t oldval;
 
-	mtx_lock(&atomic_mtx);
+	mtx_lock(&atomic_lock);
 	oldval = *target;
 	if (oldval == cmp)
 		*target = newval;
-	mtx_unlock(&atomic_mtx);
+	mtx_unlock(&atomic_lock);
 	return (oldval);
 }
 
