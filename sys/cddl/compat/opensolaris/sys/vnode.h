15109d72e819a31555c470641543a0e6bf89f7a4
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-10-01 12:34:05 +1000

    sys/cddl: NOP-out vn_mountedvfs() and vn_ismntpt() for the moment
    REVIST
    
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- sys/cddl/compat/opensolaris/sys/vnode.h
+++ sys/cddl/compat/opensolaris/sys/vnode.h
@@ -75,8 +75,10 @@ vn_is_readonly(vnode_t *vp)
 }
 #define	vn_vfswlock(vp)		(0)
 #define	vn_vfsunlock(vp)	do { } while (0)
-#define	vn_ismntpt(vp)		((vp)->v_type == VDIR && (vp)->v_mountedhere != NULL)
-#define	vn_mountedvfs(vp)	((vp)->v_mountedhere)
+//#define	vn_ismntpt(vp)		((vp)->v_type == VDIR && (vp)->v_mountedhere != NULL)
+#define	vn_ismntpt(vp)		(0)
+//#define	vn_mountedvfs(vp)	((vp)->v_mountedhere)
+#define	vn_mountedvfs(vp)	(0)
 #define	vn_has_cached_data(vp)	\
 	((vp)->v_object != NULL && \
 	 ((vp)->v_object->resident_page_count > 0 || \
